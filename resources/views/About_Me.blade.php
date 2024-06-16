527251bbcac6cf021dfbba0c49ef8901f1037e95
Emerson-Bagawe <eob2286@students.uc-bcf.edu.ph>
2024-06-16 18:40:46 +0800

    Created 3 pages with buttons on each page that routes to specific pages

---
+++ resources/views/About_Me.blade.php
@@ -0,0 +1,8 @@
+<div style="display:flex; width:99vw; height:10vh; justify-content:center; "  >
+    <h1>About Me Page</h1>
+</div>
+    <div style="display:flex; justify-content:space-evenly"> 
+    <a href='/About'><button class="btn btn-info"> About Me </button></a>
+    <a href='/Skills'><button class="btn btn-primary"> Skills </button></a>
+    <a href='/Hobbies'><button class="btn btn-secondary"> Hobbies </button></a>
+    </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,18 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/About', function() {
+    return view('About_Me');
+});
+
+Route::get('/Skills', function() {
+    return view('Skills');
+});
+
+Route::get('/Hobbies', function() {
+    return view('Hobbies');
+});
+
 Route::get('/', function () {
     return view('welcome');
 });
